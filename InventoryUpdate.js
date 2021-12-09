12f45e25560c8bf6ff8834dd41194d1edef39ed2
EddyBautista <bautista.j.eddy@gmail.com>
2021-12-09 06:56:06 -0600

    completed inventoryUpdate

--- InventoryUpdate.js
+++ InventoryUpdate.js
@@ -1,11 +1,24 @@
 const updateInventory = (arr1, arr2) => {
-    // Loop throw the current inventory 
-    for (let index = 0; index < arr1.length; index++) {
-        const element = arr1[index];
-        console.log(element);
-        
+
+    for(var i = 0; i< arr2.length; i++){
+        //check to see if inventory currently exist in the curInv
+        var inventoryExist = arr1.every(function(e){
+            return arr2[i][1] != e[1]
+        })
+        if(inventoryExist){ // add if doesn't exist
+            arr1.push(arr2[i]); 
+        } else{
+            // iterate where this item exist and finds the index of same item
+            var indexVal = arr1.findIndex(function(e){
+                return e[1] === arr2[i][1];
+            })
+            arr1[indexVal][0] += arr2[i][0];
+        }
     }
+    return arr1.sort(function(a,b){return a[1] <b[1] ? -1:1;});
 }
+
+
 
 // Example inventory lists
 var curInv = [
@@ -22,4 +35,4 @@ var newInv = [
     [7, "Toothpaste"]
 ];
 
-updateInventory(curInv, newInv);
+console.log(updateInventory(curInv, newInv));
